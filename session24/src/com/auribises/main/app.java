dac2f66f799bc0c3f00a50e2d76ad27167e14f76
Baljit S Khare <Baljit S Khare@BaljitSKhare-PC>
2019-02-20 23:47:57 +0530

    app

---
+++ session24/src/com/auribises/main/app.java
@@ -0,0 +1,15 @@
+  package com.auribises.main;
+
+  import com.auribises.view.AddCustomerwindow;
+
+  public class app {
+	  //main method : represent main thread !!
+	  //Execution of Program begins here !!
+	  public static void main(String[] args) {
+		  
+		  	AddCustomerwindow  window = new AddCustomerwindow();
+		  	window.showWindow();
+	}
+	
+
+}
